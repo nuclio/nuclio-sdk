3f61b4db7926956a5af6cd0087422708f21ace0b
Miki Tebeka <miki.tebeka@gmail.com>
2017-08-21 14:40:54 +0300

    Docs and example

--- examples/os-packages/install.sh
+++ examples/os-packages/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Example install script. This will run on the processor container
+# You need to provide path to this file in build.yaml
 
 apt update
 apt install -y --no-install-recommends \
