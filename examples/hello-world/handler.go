7ba30bf581fe4b9dcc2b2c526dbec9a01259b8d9
Miki Tebeka <miki.tebeka@gmail.com>
2017-09-14 19:14:13 +0300

    sync with vendored sdk

--- examples/hello-world/handler.go
+++ examples/hello-world/handler.go
@@ -2,21 +2,12 @@
 // a Handler() function in the handler package. Future implementations
 // will alleviate this limitation
 
-// To build the handler run
-//	go build -buildmode=plugin -o handler.so handler.go
-
-package main
+package handler
 
 import (
 	"github.com/nuclio/nuclio-sdk"
 )
 
-var (
-// Make sure our handler has the right signature
-//	_ = nuclio.EventHandler(Handler)
-)
-
-// Handler is the event Handler
 func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 	context.Logger.Info("Event received")
 
@@ -26,5 +17,3 @@ func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 		Body:        []byte("Response from handler"),
 	}, nil
 }
-
-func main() {}
